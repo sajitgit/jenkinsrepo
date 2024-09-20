09e799fde35bc93bd4590d384fa6cc182f37a3c7
hp <hp@DESKTOP-8QRD8C9.bbrouter>
2024-09-20 11:12:51 +0530

    Jenkin files commited

---
+++ jenkinsample/src/main/java/jenkinpackage/JenkinClass.java
@@ -0,0 +1,10 @@
+package jenkinpackage;
+
+public class JenkinClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
